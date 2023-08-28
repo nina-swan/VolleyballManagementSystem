976a8f3b11257fafeaa6a4a876e45bae57d4f842
Nina <nina.labecka@gmail.com>
2023-08-28 18:19:09 +0200

    added html+css template added articles, profile, register pages

---
+++ VolleyballWebCore/VolleyballBlazor.Client/AuthStateProvider.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Options;
+using System.Security.Principal;
+using Blazored.LocalStorage;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace VolleyballBlazor.Client
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+        private readonly AuthenticationState _anonymous;
+
+        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _localStorage = localStorage;
+            _anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorage.GetItemAsync<string>("authToken");
+
+            if (string.IsNullOrWhiteSpace(token))
+                return _anonymous;
+
+            try
+            {
+                IIdentity identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwtAuthType");
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+                return new AuthenticationState(new ClaimsPrincipal(identity));
+            }
+            catch
+            {
+                return _anonymous;
+            }
+        }
+
+        public void NotifyUserAuthentication(string token)
+        {
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(token), "jwtAuthType"));
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public void NotifyUserLogout()
+        {
+            var authState = Task.FromResult(_anonymous);
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var a = new JwtSecurityToken(jwt);
+            return a.Claims;
+        }
+    }
+
+}
--- VolleyballWebCore/VolleyballBlazor.Client/Program.cs
+++ VolleyballWebCore/VolleyballBlazor.Client/Program.cs
@@ -1,11 +1,21 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using VolleyballBlazor.Client;
+using VolleyballBlazor.Infrastructure.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+builder.Services.AddSingleton<UserService>();
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
