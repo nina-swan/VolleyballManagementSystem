1855e38f4f001985c97d096e60621f9fad50bc3f
Nina <nina.labecka@gmail.com>
2023-08-28 18:00:26 +0200

    change project structure

--- VolleyballWebCore/Volleyball.Api/Program.cs
+++ VolleyballWebCore/Volleyball.Api/Program.cs
@@ -1,8 +1,53 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Volleyball.Infrastructure.Database.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<VolleyballSysDbContext>(options =>
+{
+    options.ConfigureWarnings(warnings =>
+    warnings.Ignore(CoreEventId.NavigationBaseIncludeIgnored));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.SetIsOriginAllowed((host) => true);
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+        }
+        );
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        //RoleClaimType = "role",
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey
+        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+    };
+});
 
 var app = builder.Build();
 
@@ -14,4 +59,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors();
 app.Run();
