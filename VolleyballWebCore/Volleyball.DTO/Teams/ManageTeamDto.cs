353aaf7a76717014caaf77e17ee8803e60e91a6d
Nina <nina.labecka@gmail.com>
2023-12-07 12:53:06 +0100

    manage team - work in progress

---
+++ VolleyballWebCore/Volleyball.DTO/Teams/ManageTeamDto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volleyball.Infrastructure.Database.Models;
+
+namespace Volleyball.DTO.Teams
+{
+    public class ManageTeamDto
+    {
+        [Display(Name = "Kapitan drużyny")]
+        public TeamPlayerDto Captain { get; set; } = null!;
+
+        public virtual List<TeamPlayerDto> Players { get; set; } = null!;
+
+        [Display(Name = "Email drużynowy")]
+        [Required(ErrorMessage = "Email jest wymagany.")]
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "Logo drużyny")]
+        public byte[]? Logo { get; set; }
+
+        [Display(Name = "Zdjęcie drużyny")]
+        public byte[]? Photo { get; set; }
+
+        [Display(Name = "Numer telefonu drużynowy")]
+        [Required(ErrorMessage = "Numer telefonu jest wymagany.")]
+        public string Phone { get; set; } = null!;
+
+        [Display(Name = "Opis drużyny")]
+        public string? TeamDescription { get; set; }
+
+        [Display(Name = "Strona internetowa")]
+        public string? Website { get; set; }
+
+        public static explicit operator ManageTeamDto(ManagedTeamDataDto team)
+        {
+            return new ManageTeamDto
+            {
+                Captain = team.Captain,
+                Email = team.Email,
+                Logo = team.Logo,
+                Photo = team.Photo,
+                Phone = team.Phone,
+                Players = team.Players,
+                TeamDescription = team.TeamDescription,
+                Website = team.Website
+            };
+        }
+    }
+}
---
+++ VolleyballWebCore/Volleyball.DTO/Teams/ManagedTeamDataDto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Volleyball.Infrastructure.Database.Models;
+
+namespace Volleyball.DTO.Teams
+{
+    public class ManagedTeamDataDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public DateTime CreationDate { get; set; }
+
+        public byte[]? Image { get; set; }
+
+        public LeagueDto? League { get; set; }
+
+        public TeamPlayerDto Captain { get; set; } = null!;
+
+        public List<TeamPlayerDto> Players { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public byte[]? Logo { get; set; }
+
+        public byte[]? Photo { get; set; }
+
+        public string Phone { get; set; } = null!;
+
+        public string? TeamDescription { get; set; }
+
+        public string? Website { get; set; }
+
+        public int? PointCorrection { get; set; }
+
+        // Method to generate TeamDto from Team
+        public static ManagedTeamDataDto GenerateTeamDto(Team team)
+        {
+            return new ManagedTeamDataDto
+            {
+                Id = team.Id,
+                Name = team.Name,
+                CreationDate = team.CreationDate,
+                Image = team.Image,
+                League = LeagueDto.GenerateLeagueDto(team.League),
+                Captain = (TeamPlayerDto)team.Captain,
+                Players = team.TeamPlayers.Select(p => (TeamPlayerDto)p).ToList(),
+                Email = team.Email,
+                Logo = team.Logo,
+                Photo = team.Image,
+                Phone = team.Phone,
+                TeamDescription = team.TeamDescription,
+                Website = team.Website,
+                PointCorrection = team.PointCorrection
+            };
+        }
+
+        public static explicit operator ManagedTeamDataDto(Team team)
+        {
+            return GenerateTeamDto(team);
+        }
+
+    }
+}
